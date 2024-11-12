35b95ecaa8d8ea4700eaa776356346bd83d5775e
RoyHC11 <roy.hcf.6@gmail.com>
2024-11-11 21:35:25 -0500

    ..

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
       <div class="copyright text-center my-auto">
-        <span>UNSA © Control de Valores de Temperatura y Humedad 2024</span>
+        <span>UNSA © DISEÑO DE UN SISTEMA DE MONITOREO UTILIZANDO TECNOLOGÍA LORA PARA GESTIONAR EL MANTENIMIENTO PREDICTIVO DE MOTORES ELÉCTRICOS DE UNA EMPRESA HOTELERA 2024</span>
       </div>
     </div>
   </footer>
